0e7529cc685fcfe2b46722c17c5bc2fabb05279e
Ratnakar <ratnakarkolhatkar@gmail.com>
2024-09-12 21:03:03 +0530

    first commit

---
+++ src/main/java/com/ratnakar/practice/TicketBookingAPI/TicketBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ratnakar.practice.TicketBookingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ratnakar/practice/TicketBookingAPI/TicketBookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratnakar.practice.TicketBookingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
